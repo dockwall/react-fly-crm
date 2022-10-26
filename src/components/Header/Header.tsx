17c9368abe7149c08ba85c1dbae8131d0ca3e24f
Stas <leshuksu@gmail.com>
2022-10-26 15:35:56 +0300

    Complete Wrench element in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Header.scss";
+import WrenchImage from "./img/Wrench.svg";
 
 const Header = () => {
   return (
     <div className="root">
       <div className="content">
-        <div className="content-label">Content label</div>
+        <div className="content-label">
+          <button type="button" className="content-label__button">
+            <img
+              className="content-label__button__img"
+              src={WrenchImage}
+              alt="Wrench"
+            />
+            Wrench CRM
+          </button>
+        </div>
         <div className="content-auth">Auth</div>
       </div>
     </div>

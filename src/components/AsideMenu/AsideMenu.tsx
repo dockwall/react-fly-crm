d3a4df74570e33b3d111c7645e0cf256253f2fa3
Stas <leshuksu@gmail.com>
2022-10-26 19:32:16 +0300

    Styles for menu is done. Menu is almost done

--- src/components/AsideMenu/AsideMenu.tsx
+++ src/components/AsideMenu/AsideMenu.tsx
@@ -1,13 +1,75 @@
 import React from "react";
 import "./AsideMenu.scss";
+import mainImage from "./img/main.svg";
+import searchImage from "./img/search.svg";
+import tablesImage from "./img/tables.svg";
+import calendarImage from "./img/calendar.svg";
+import mapsImage from "./img/maps.svg";
+import widgetsImage from "./img/widgets.svg";
+import settingsImage from "./img/settings.svg";
+import exitImage from "./img/exit.svg";
 
 const AsideMenu = () => {
   return (
     <div className="menu-root">
       <div className="menu-content">
-        <div className="menu-content-element">1</div>
-        <div className="menu-content-element">2</div>
-        <div className="menu-content-element">3</div>
+        <p className="menu-content-title">Меню</p>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={mainImage} alt="Главная" />
+          <span>Главная</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={searchImage} alt="Поиск адресов" />
+          <span>Поиск адресов</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={tablesImage} alt="Таблицы" />
+          <span>Таблицы</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={calendarImage} alt="Календарь" />
+          <span>Календарь</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element cards"
+        >
+          <img src={mapsImage} alt="Карты" className="cards__image" />
+          <span>Карты</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={widgetsImage} alt="Виджеты" />
+          <span>Виджеты</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={settingsImage} alt="Настройки" />
+          <span>Настройки</span>
+        </button>
+        <button
+          type="button"
+          className="menu-content__button menu-content-element"
+        >
+          <img src={exitImage} alt="Выход" />
+          <span>Выход</span>
+        </button>
       </div>
     </div>
   );

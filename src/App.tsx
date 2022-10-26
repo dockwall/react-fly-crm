02166ff6997e0da7f8f84dd6450121f0a29948e8
Stas <leshuksu@gmail.com>
2022-10-26 17:19:18 +0300

    Started working with menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./assets/styles/index.scss";
 import Header from "./components/Header/Header";
+import AsideMenu from "./components/AsideMenu/AsideMenu";
 
 const App = () => {
   return (
     <React.StrictMode>
       <Header />
+      <AsideMenu />
       <div>Hello!</div>
     </React.StrictMode>
   );
---
+++ src/components/AsideMenu/AsideMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AsideMenu = () => {
+  return <div>AsideMenu</div>;
+};
+
+export default AsideMenu;

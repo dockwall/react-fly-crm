7998fa0e276d3a155a98e48bb1a75f8e3e30c84c
Stas <leshuksu@gmail.com>
2022-10-27 17:51:57 +0300

    Completed form

--- src/components/Content/Address/Address.tsx
+++ src/components/Content/Address/Address.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./Address.scss";
+import SearchImage from "./img/Search.svg";
 
 const Address = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="address-root">
+      <h1>Поиск адресов</h1>
+      <form className="address-form">
+        <fieldset className="form-fieldset">
+          <label htmlFor="address" className="form__label">
+            Введите интересующий вас адрес
+          </label>
+          <div className="form-ui">
+            <input
+              className="form__input"
+              type="text"
+              name="address"
+              minLength={3}
+              placeholder="Введите интересующий вас адрес"
+            />
+            <button type="submit" className="form-submit__button">
+              <img src={SearchImage} alt="Поиск" className="search-icon" />
+              Поиск
+            </button>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 };

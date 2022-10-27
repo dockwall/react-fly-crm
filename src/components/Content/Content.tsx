294f69563c65a52ad4b2d9aeb52e61910ef2d385
Stas <leshuksu@gmail.com>
2022-10-27 15:19:57 +0300

    Started correct news block

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import AsideMenu from "./AsideMenu/AsideMenu";
 import "./Content.scss";
+import News from "./News/News";
 
 const Content = () => {
   return (
     <div className="content-root">
       <AsideMenu />
 
-      <div className="content-block">Hello!</div>
+      <div className="content-block">
+        <News />
+      </div>
     </div>
   );
 };
---
+++ src/components/Content/News/News.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./News.scss";
+
+const News = () => {
+  return <div className="news-root">Hello!</div>;
+};
+
+export default News;
